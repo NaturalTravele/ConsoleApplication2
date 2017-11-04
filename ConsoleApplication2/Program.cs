806cbce2115e32c110f4770ca0ca36406591e474
Administrator <Administrator@USER-20170915UI>
2017-11-04 17:24:59 +0800

    添加项目文件。

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Rectangle
+    {
+        internal double length;
+        internal double width;
+
+        double GetArea()
+        {
+            return length * width;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine();
+        }
+    }
+}
